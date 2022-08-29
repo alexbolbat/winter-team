aaabe43bc65d8199b269eee7bcc56ffc6c0e7834
Iryna <79843845+shumakovaira@users.noreply.github.com>
2022-08-29 13:23:48 +0300

    styled header

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <the-header />
+    <br />
     <router-view />
   </div>
 </template>
@@ -14,14 +15,3 @@ export default {
   }
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-</style>
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,18 +1,42 @@
 <template>
   <div>
-    <h2>
-      <a href="/">
-        Home
-      </a>
-    </h2>
-    <input
-      v-model="queryValue"
-      type="text"
-      class="searchInput"
+    <v-toolbar 
+      height="80"
+    >
+      <v-toolbar-title class="text-uppercase">
+        <a href="/">
+          <img 
+            src="../assets/Logo.svg" 
+            alt="logo"
+          />
+        </a>
+      </v-toolbar-title>
+      <v-spacer />
+      <!-- <v-text-field
+        v-model="queryValue"
+        label="find your film"
+        solo
+        hide-details="auto"
+      /> -->
+      <input
+        v-model="queryValue" 
+        type="text" 
+        class="inputField"
+        placeholder="find your film here"
+      />
+      <v-btn
+        height="35px"
+        small
+        @click="queryInput"
+      >
+        Search
+      </v-btn>
+    </v-toolbar>
+    
+    <img 
+      class="banner" 
+      src="../assets/main page banner.svg" 
     />
-    <button @click="queryInput">
-      Search
-    </button>
   </div>
 </template>
 
@@ -32,3 +56,52 @@ export default {
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.theme--light.v-toolbar.v-sheet {
+    background-color: #6bc5da;
+};
+.theme--light.v-btn.v-btn--has-bg {
+  margin-left: -10px;
+  color: #ffffff;
+  background-color: #22748c;
+  box-shadow: none;
+  border: 1px solid #22748c;
+}
+.theme--light.v-btn.v-btn--has-bg:hover {
+    background-color: #a2dfec;
+    opacity: 1;
+    color: #22748c;
+    transition: ease-in 0.25s;
+}
+
+.v-sheet.v-toolbar:not(.v-sheet--outlined) {
+  box-shadow: none;
+}
+
+.inputField{
+  height: 35px;
+  background-color: #ffffff;
+  width: 450px;
+  padding-left: 5px;
+  border-top-left-radius: 5px;
+  border-bottom-left-radius: 5px;
+  color:#6bc5da;
+  border: 1px solid #22748c;
+}
+
+input[type="text"]:hover, input[type="text"]:focus {
+  outline-offset: none;
+  outline: none;
+}
+
+input[type="text"]::placeholder{
+  color: #6bc6dab7;
+}
+.banner{
+  margin-top: 5px;
+  display: block;
+  margin-left: auto;
+  margin-right: auto
+}
+</style>
