2282b5478f95cae66d941ee628bcea739102a36f
Iryna <79843845+shumakovaira@users.noreply.github.com>
2022-08-30 20:53:33 +0300

    search by id

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import popularMovies from './modules/popularMovies';
 import searchMovies from './modules/searchMovies';
+import singleMovie from './modules/singleMovie';
+import singlePerson from './modules/singlePerson';
 
 
 Vue.use(Vuex);
@@ -10,5 +12,7 @@ export default new Vuex.Store({
   modules: {
     popularMovies,
     searchMovies,
+    singleMovie,
+    singlePerson,
   }
 });
