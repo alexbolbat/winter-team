ff238bae3f1d3424748fa1bee2913e18f076df58
Yakovlev Andriy <yk.120301@gmail.com>
2022-09-01 12:26:26 +0300

    [FIX] pagination replaced

--- src/components/MoviesList.vue
+++ src/components/MoviesList.vue
@@ -1,14 +1,5 @@
 <template>
   <div>
-    <span>
-      <v-pagination
-        :value="page"
-        :length="500"
-        :total-visible="7"
-        class="mb-7"
-        @input="choosePage"
-      />
-    </span>
     <v-container class="d-flex flex-wrap justify-space-between">
       <movie-list-item
         v-for="item in movies"
@@ -16,6 +7,13 @@
         :item="item"
       />
     </v-container>
+    <v-pagination
+      :value="page"
+      :length="500"
+      :total-visible="7"
+      class="mb-7"
+      @input="choosePage"
+    />
   </div>
 </template>
 
