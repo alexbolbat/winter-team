f9656795c22161f8454e480fdefec84f9c08336f
Yakovlev Andriy <yk.120301@gmail.com>
2022-09-09 19:19:13 +0300

    [FIX] some design fix

--- src/components/MoviesList.vue
+++ src/components/MoviesList.vue
@@ -1,3 +1,4 @@
+<!-- eslint-disable vue/singleline-html-element-content-newline -->
 <template>
   <div v-if="movies.length && !isLoading">
     <v-container class="d-flex flex-wrap justify-space-between pa-0 pt-3">
@@ -15,12 +16,7 @@
       @input="choosePage"
     />
   </div>
-  <v-container
-    v-else-if="!isLoading"
-    class="d-flex flex-wrap justify-space-between pa-0 pt-3 text-h2"
-  >
-    No results
-  </v-container>
+
   <v-container
     v-else-if="isLoading"
     class="d-flex flex-wrap justify-space-between pa-0 pt-3 text-h2"
@@ -29,9 +25,26 @@
       v-for="i in 8"
       :key="i"
       width="242"
-      height="468"
+      height="495"
       type="image, article"
     />
+  </v-container>
+  <v-container
+    v-else-if="!isLoading"
+    class="d-flex flex-column align-center pa-0 pt-3"
+  >
+    <img
+      class="mt-3"
+      width="774"
+      height="337"
+      src="../assets/no-results.svg"
+      alt="no-results"
+    />
+    <div class="text-h2 font-weight-bold mt-2">Oops!</div>
+    <p class="text-h4 mt-4 error-text">
+      Sorry, we can`t find the film you are searching for but you always can
+      come back to main page
+    </p>
   </v-container>
 </template>
 
@@ -66,5 +79,9 @@ export default {
 }
 .v-skeleton-loader__image {
   height: 317px;
+}
+.error-text {
+  width: 575px;
+  text-align: center;
 }
 </style>
