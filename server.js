e3ffb48fe2e6e3e420c3947aa1e2d3e6d3aa3aa7
Alexander Bolbat <zoeabll@ericsson.com>
2022-09-14 16:02:20 +0300

    fixed path #2

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const serveStatic = require('serve-static');
+const path = require('path');
+app = express();
+app.use(serveStatic(path.join(__dirname, 'dist')));
+app.get('/', function (req,res) {
+  res.sendFile(path + "index.html");
+});
+const port = process.env.PORT || 3000;
+app.listen(port);
+
+
+
+
+const express = require('express');
 const path = require('path');
 const history = require('connect-history-api-fallback');
 
 const app = express();
 
-const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
+const staticFileMiddleware = express.static(path.join(__dirname + 'dist'));
 
 app.use(staticFileMiddleware);
 app.use(history({
@@ -14,7 +28,9 @@ app.use(history({
 app.use(staticFileMiddleware);
 
 app.get('/', function (req, res) {
-  res.render(path.join(__dirname + '/index.html'));
+  // res.render(path.join(__dirname + '/dist/index.html'));
+  
+  res.render(path.join(path + "index.html"));
 });
 
 const server = app.listen(process.env.PORT || 3000, function () {
