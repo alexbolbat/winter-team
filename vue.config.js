d3476bb56d4a8cb8ea6bc6decb19c73a35f5079d
Alexander Bolbat <zoeabll@ericsson.com>
2022-09-14 13:04:14 +0300

    updated build

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? 'winter-team' : '/',
+  // publicPath: process.env.NODE_ENV === 'production' ? 'winter-team' : '/',
 
   pages: {
     index: {
