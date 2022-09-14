7ea6e36267804fbf08882888b66f319fcb76743c
Alexander Bolbat <zoeabll@ericsson.com>
2022-09-14 12:52:42 +0300

    updated build/ added public path

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,23 @@
 const path = require('path');
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? 'winter-team' : '/',
+
   pages: {
     index: {
       entry: 'src/main.js',
-      title: 'Winter films',
+      title: 'Winter films'
     }
   },
 
   chainWebpack: config => {
     const types = ['vue-modules', 'vue', 'normal-modules', 'normal'];
-    types.forEach(type => addStyleResource(config.module.rule('sass').oneOf(type)));
+    types.forEach(type =>
+      addStyleResource(config.module.rule('sass').oneOf(type))
+    );
   },
 
-  transpileDependencies: [
-    'vuetify'
-  ],
+  transpileDependencies: ['vuetify'],
 
   pluginOptions: {
     'style-resources-loader': {
@@ -25,12 +27,11 @@ module.exports = {
   }
 };
 
-function addStyleResource (rule) {
-  rule.use('style-resource')
+function addStyleResource(rule) {
+  rule
+    .use('style-resource')
     .loader('style-resources-loader')
     .options({
-      patterns: [
-        path.resolve(__dirname, 'src/styles/index.scss')
-      ]
+      patterns: [path.resolve(__dirname, 'src/styles/index.scss')]
     });
 }
